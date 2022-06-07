0b1061ce677d99664bd56c72ed77c50fe98d3819
luconde@outlook.com <luconde@outlook.com>
2022-06-07 15:01:42 -0300

    Release V1.0

---
+++ FnSomar/Function1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FnSomar
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string primeiro = req.Query["primeiro"];
+            string segundo = req.Query["segundo"];
+
+            int terceiro = int.Parse(primeiro) + int.Parse(segundo);
+
+            return new OkObjectResult(terceiro.ToString());
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Visão Geral
+Este projeto tem o objetivo de demonstrar a criação e publicação de uma Azure Function através do Visual Studio 2022
+
+# Autor
+Luciano Condé de Souza (luconde@gmail.com)
+Data: 2022-05-26
+
+# Detalhes técnicos
+
+## Funcionalidades
+1. Uma função chamada "Function1" que receberá dois parâmetros através de uma ativação Post para realizar uma soma
+
+## Pré-requisitos
+1. Azure Function
+2. Subscription Ativa do Microsoft Azure
+3. Visual Studio 2022 Community para executar o código-fonte
